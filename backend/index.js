393690d7d2c4601d04f4a247dbb4f9fcb06bfc45
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2022-07-29 11:31:07 -0700

    added models, controllers and routes

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyparser from "body-parser";
+
+
+const app = express();
+const PORT =3000;
+// mongo connection
+mongoose.Promise= global.Promise;
+mongoose.connect('mongodb://localhost/soccerDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// bodyparser set up
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
+
+app.get('/', (req, res) =>
+res.send(`Our soccer application is running on port ${PORT}`));
+
+app.listen(PORT,() =>
+console.log(`Your soccer server is running on port ${PORT}`));
